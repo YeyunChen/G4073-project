e3c7519e3885eef24fdb31a4bdedd01d934c7b79
YeyunChen <YeyunChen@outlook.com>
2015-11-09 17:48:43 -0500

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -46,3 +46,59 @@ df['ema']    = get_ema(df['Price'])
 df['mom10']  = get_mom(df['Price'])
 df['momEMA'] = get_mom_ema(df['Price'])
 df['bollUpper'], df['bollLower'] = get_bollinger(df['Price'])
+
+# ----------------------------------------
+# More indicators by Yeyun Chen
+
+# Acceleration (difference of price change)
+def get_accel(df_col, per = 10):
+    mom_t = get_mom(df_col, per)[per:]
+    roll = (np.roll(mom_t, 0)-np.roll(mom_t, 1))[1: ]
+    fill = [np.nan]*(per+1)
+    return pd.Series(fill+list(roll))
+  
+# rate of change  
+def get_roc(df_col, per = 10):
+  roll = ((get_mom(df_col, per))/(np.roll(df_col, per)))[per:]
+  fill = [np.nan]*per
+  return pd.Series(fill+list(roll))
+
+# moving average convergence divergence
+# difference between two moving avergaes of slow(s) and fast(f) periods
+def get_macd(df_col, s = 26, f = 12):
+    return(pd.ewma(df_col, span = s)-pd.ewma(df_col, span = f))
+
+# MACD signal line: moving average of MACD of past n periods.
+def get_macds(df_col, s = 26, f = 12, n = 9):
+    return(pd.ewma(get_macd(df_col, s, f), span = n))
+
+# Relative strength index
+def get_rsi(df_col, per = 10):
+    p_up = [np.nan]*df_col.size
+    p_dn = [np.nan]*df_col.size
+    for i in range(1, df_col.size):
+        if (df_col[i] - df_col[i-1] > 0):
+            p_up[i] = df_col[i]
+        elif (df_col[i] - df_col[i-1] < 0):
+            p_dn[i] = df_col[i]
+
+    ma_up = [np.nan]*df_col.size
+    ma_dn = [np.nan]*df_col.size
+    rsi = [np.nan]*df_col.size
+    for j in range(per+1, df_col.size):
+        ma_up[j] = np.nanmean(p_up[j-per:j])
+        ma_dn[j] = np.nanmean(p_dn[j-per:j])
+        rsi[j] = 100-100/(1+ma_up[j]/ma_dn[j])
+    fill = [np.nan]*(per+1)
+    return pd.Series(fill+list(rsi))
+        
+
+# The above functions take an array as an argument, with other optional parameters
+# Each function returns an array, which can be used in other functions
+
+# Add columns to DataFrame
+df['accel'] = get_accel(df['Price'])
+df['roc'] = get_roc(df['Price'])
+df['macd'] = get_macd(df['Price'])
+df['macds'] = get_macds(df['Price'])
+df['rsi'] = get_rsi(df['Price'])
